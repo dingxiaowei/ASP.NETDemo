37acb8aff132aa709f2df8703cf6bc9200a14c4e
d00605132 <dingxiaowei2@huawei.com>
2022-04-23 20:59:37 +0800

    更新

---
+++ WebForm/Stock.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace WebForm
+{
+    public partial class Stock : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            ViewState["BaseDir"] = Path.Combine(baseDir, "Excels/");
+        }
+
+        //股票搜索
+        protected void BtnSearch_Click(object sender, EventArgs e)
+        {
+            string excelDir = ViewState["BaseDir"].ToString();
+            string stockInfo = ttbStock.Text;
+            if (string.IsNullOrEmpty(stockInfo))
+            {
+                Response.Write("<script>alert('请输入要搜索的股票信息')</script>");
+                return;
+            }
+            DirectoryInfo dirInfo = new DirectoryInfo(excelDir);
+            var files = dirInfo.GetFiles().Where(s => s.Name.Contains(stockInfo)).ToList();
+            if(files.Count < 1)
+            {
+                Response.Write("<script>alert('没有搜索到相关股票数据')</script>");
+                return;
+            }
+            Response.Write(files[0].Name);
+        }
+    }
+}
---
+++ WebForm/obj/Release/Package/PackageTmp/Stock.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Stock.aspx.cs" Inherits="WebForm.Stock" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="ttbStock" runat="server"></asp:TextBox>
+            <asp:Button ID="BtnSearch" runat="server" Text="搜索" OnClick="BtnSearch_Click" />
+            
+            <br />
+            <br />
+            <br />
+            <label>Powerd By 阿拉丁(微信:AladdinDXW)</label>
+        </div>
+    </form>
+</body>
+</html>
