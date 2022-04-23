8ee41fb50438e29cde7903959597470eaf35dbbc
dingxiaowei <1213250243@qq.com>
2022-04-23 17:23:43 +0800

    第一次访问页面

--- WebForm/Index.aspx.cs
+++ WebForm/Index.aspx.cs
@@ -11,7 +11,10 @@ namespace WebForm
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //if (!IsPostBack)
+            //{
+            //    Response.Write("第一次加载");
+            //}
         }
         protected void BtnConsole_Click(object sender, EventArgs e)
         {
