d18ee854f0f297e33417bd56a5f053dc65c634df
dingxiaowei <1213250243@qq.com>
2022-04-23 16:17:52 +0800

    可以发布成功,如果显示MIME的问题，则查看一下ISAPI和CGUI限制看是否有V4.0的环境

---
+++ WebForm/Index.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForm
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void BtnConsole_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(TxtBox1.Text);
+            int num2 = Convert.ToInt32(TxtBox2.Text);
+            int sum = num1 + num2;
+            TxtBox3.Text = sum.ToString();
+        }
+    }
+}
